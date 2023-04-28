34ceb4d0cab088076c18d7c5ef1eb9ba86683e6a
njuguna199 <kimaninjuguna199@gmail.com>
2023-04-28 11:21:50 +0000

    0x15

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 """
-Using https://jsonplaceholder.typicode.com
-returns info about employee TODO progress
-Implemented using recursion
+This script uses the JSONPlaceholder API to retrieve information about a user's todo list progress.
+
+Usage: ./0-gather_data_from_an_API.py [employee_id]
 """
-import re
-import requests
 import sys
-
-
-API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
+import requests
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        employee_id = int(sys.argv[1])
+        user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+        todos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
+
+        try:
+            employee_name = user_response.json().get('name')
+            todos = todos_response.json()
+            total_tasks = len(todos)
+            done_tasks = [t for t in todos if t.get('completed')]
+            num_done_tasks = len(done_tasks)
+
+            print("Employee {} is done with tasks({}/{}):".format(employee_name, num_done_tasks, total_tasks))
+
+            for task in done_tasks:
+                print("\t {}".format(task.get('title')))
+        except ValueError:
+            print('Invalid JSON')
+    else:
+        print('Usage: ./0-gather_data_from_an_API.py [employee_id]')
+
